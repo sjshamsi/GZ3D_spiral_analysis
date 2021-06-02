c086ea2eb81719a363fb58bfb8e6e8e7e7e672a2
SJShamsi <sjs.shamsi@gmail.com>
2021-06-02 21:25:15 +0000

    Consolidating changes in the VM so I can commit them on the github.

--- spiral_galaxy.py
+++ spiral_galaxy.py
@@ -1,17 +1,21 @@
+#First we'll import all the module we need
 from marvin.tools.maps import Maps
 
+import pathlib
 import pandas as pd
 import numpy as np
 from numpy.linalg import multi_dot
 
 import sys
-sys.path.insert(0, '/home/sshamsi/spirals/GZ3D_production')
+sys.path.insert(0, '/home/sshamsi_haverford_edu/galaxy_zoo/GZ3D_production') #this might need changing if working across platforms
 
 import gz3d_fits
 
+# The Spiral Galaxy class is defined below
 class SpiralGalaxy(object):
     def __init__(self, file_path):
         self.file_path = file_path
+        self.filename = self.file_path.split('/')[-1]
         self.data = gz3d_fits.gz3d_fits(file_path)
         self.mangaid = self.data.metadata['MANGAID'][0]
         
@@ -35,19 +39,10 @@ class SpiralGalaxy(object):
         self.global_df_loaded = False
         self.bpt_masks_loaded = False
         self.r_array_loaded = False
-        self.corr_matrix_loaded = False
-        self.ha_cov_loaded = False
-        self.hb_cov_loaded = False
         
         
     def __repr__(self):
         return 'MaNGA ID {}'.format(self.mangaid)
-    
-    
-    def form_correllation_matrix(self):
-        if not self.corr_matrix_loaded:
-            self.corr_matrix = np.load('corr_matrices/corr_matrix' + str(self.map_shape[0]) + '.npy')
-            self.corr_matrix_loaded = True
             
     
     def check_usability(self, threshold = 5, pix_percentage = 1.5):
@@ -61,6 +56,10 @@ class SpiralGalaxy(object):
     
     
     def make_emmasks(self):
+        '''Takes masks from the MaNGA maps and if a spaxel is flagges as
+        "DO NOT USE (2**30) then it marks it as "0". Creates global maek objects
+        from these.'''
+        
         self.ha_mask_array = self.hamap.mask.flatten()
         self.hb_mask_array = self.hbmap.mask.flatten()
         
@@ -80,6 +79,8 @@ class SpiralGalaxy(object):
         
         
     def make_r_array(self):
+        '''Goes through all spaxels and creates a global array of spaxel distances from the map centre'''
+        
         if not self.r_array_loaded:
             r_array = np.array([])
 
@@ -101,6 +102,9 @@ class SpiralGalaxy(object):
     
         
     def update_spirals(self, spiral_threshold=3, other_threshold=3, ret_spiral_bool=False):
+        '''Do you want to change the parametres for what does/doesn't count as a spaxel or a
+        non-spaxel? Use this function to simply update the "Spiral" column of the global DF'''
+        
         self.data.make_all_spaxel_masks(grid_size = self.map_shape)
         
         center_mask_spaxel_bool = self.data.center_mask_spaxel > other_threshold
@@ -120,6 +124,9 @@ class SpiralGalaxy(object):
         
     
     def load_btp_masks(self):
+        '''Simple make arrays of indicating the BPT classification of each spxel.
+        Then we append this to the global DF later.'''
+        
         if not self.bpt_masks_loaded:
             bpt_masks = self.maps.get_bpt(return_figure=False, show_plot=False)
 
@@ -131,21 +138,28 @@ class SpiralGalaxy(object):
             self.bpt_masks_loaded = True
     
     
-    def flux2sfr(self, ha_flux, ha_stdv, hb_flux, hb_stdv):
+    def flux2sfr(self, ha_flux, ha_stdv, hb_flux, hb_stdv, avg=False):
+        '''Take an H-alpha and H-beta flux, and then make an SFR measurement out of them.'''
+        
         ha_flux = ha_flux * 1E-13
         hb_flux = hb_flux * 1E-13
         
         ha_stdv = ha_stdv * 1E-13
         hb_stdv = hb_stdv * 1E-13
         
-        sfr = (self.delta * (ha_flux**3.36) * (hb_flux**-2.36)) / self.spax_area
+        sfr = (self.delta * (ha_flux**3.36) * (hb_flux**-2.36))
         sfr_stdv = np.sqrt((3.36 * self.delta * (ha_flux**2.36) * (hb_flux**-2.36) * ha_stdv)**2 +
-                           (-2.36 * self.delta * (ha_flux**3.36) * (hb_flux**-3.36) * hb_stdv)**2) / self.spax_area
+                           (-2.36 * self.delta * (ha_flux**3.36) * (hb_flux**-3.36) * hb_stdv)**2)
+        
+        if avg:
+            return sfr / self.spax_area, sfr_stdv / self.spax_area
         
         return sfr, sfr_stdv
     
     
     def form_global_df(self, spiral_threshold=3, other_threshold=3):
+        '''Make a global DF.'''
+        
         if not self.global_df_loaded:
             self.load_btp_masks()
             self.make_r_array()
@@ -190,6 +204,10 @@ class SpiralGalaxy(object):
             
             
     def cov_matrix_maker(self, err_series):
+        '''Calculates the covaraince matrix for our galaxy.That is rather intensive.'''
+        
+        corr_matrix = np.load('/raid5/homes/sshamsi/galaxy_zoo/GZ3D_spiral_analysis/Matrices/corr_matrices/corr_matrix' + str(self.map_shape[0]) + '.npy')
+        
         r = self.hamap.size
         cov_mat = np.zeros((r, r))
         
@@ -199,37 +217,60 @@ class SpiralGalaxy(object):
             else:
                 k = frame
                 
-            cov_mat[item] = self.corr_matrix[item] * k
-            cov_mat[:, item] = self.corr_matrix[:, item] * k
+            cov_mat[item] = corr_matrix[item] * k
+            cov_mat[:, item] = corr_matrix[:, item] * k
         
         return cov_mat
     
     
     def make_cov_matrices(self, mode=None):
-        self.form_correllation_matrix()
+        '''This method loads and returns the H-a/H-b covariance matrix. If not available,
+        it calculates it, which is resource intensive.'''
         
         if mode == None:
             raise ValueError('Argument "mode" must be set to "ha" or "hb".')
             
         elif mode == 'ha':
-            if not self.ha_cov_loaded:
-                self.ha_cov = self.cov_matrix_maker(self.df['$\sigma H_{\\alpha}$'])
-                self.ha_cov_loaded = True
+            hafile = pathlib.Path("Matrices/cov_matrices/" + self.filename.split('.')[0] + 'ha.npy')
+            print(hafile)
+            
+            if hafile.exists():
+                ha_cov = np.load(hafile)
+                return ha_cov
+            
+            else:
+                print ("H-a covariance file does not exist. Calculating...")
+                ha_cov = self.cov_matrix_maker(self.df['$\sigma H_{\\alpha}$'])
+                return ha_cov
         
         elif mode == 'hb':
-            if not self.hb_cov_loaded:
-                self.hb_cov = self.cov_matrix_maker(self.df['$\sigma H_{\\beta}$'])
-                self.hb_cov_loaded = True
-                
-                
+            hbfile = pathlib.Path("Matrices/cov_matrices/" + self.filename.split('.')[0] + 'hb.npy')
+            print(hbfile)
+            
+            if hbfile.exists():
+                hb_cov = np.load(hbfile)
+                return hb_cov
+            
+            else:
+                print ("H-b covariance file does not exist. Calculating...")
+                hb_cov = self.cov_matrix_maker(self.df['$\sigma H_{\\beta}$'])
+                return hb_cov
+            
+                            
     def get_sfr(self, index, avg=False):
+        '''Return the SFR for a bin of spxels.'''
+        
         ha_flux, ha_stdv = self.get_emission(index, mode='ha', avg=avg)
         hb_flux, hb_stdv = self.get_emission(index, mode='hb', avg=avg)
         
-        return self.flux2sfr(ha_flux, ha_stdv, hb_flux, hb_stdv)
+        return self.flux2sfr(ha_flux, ha_stdv, hb_flux, hb_stdv, avg=avg)
     
     
     def get_emission(self, index, mode=None, avg=False):
+        '''Return the H-a or H-b flux.'''
+        
+        self.form_global_df()
+        
         set_index = set(index)
         tot_index = list(self.df.index)
         
@@ -240,13 +281,13 @@ class SpiralGalaxy(object):
         elif mode == 'ha':
             summ = self.df.loc[index.tolist(), '$H_{\\alpha}$'].sum()
             
-            self.make_cov_matrices(mode=mode)
-            cov_mat = self.ha_cov
+            ha_cov = self.make_cov_matrices(mode=mode)
+            cov_mat = ha_cov
         elif mode == 'hb':
             summ = self.df.loc[index.tolist(), '$H_{\\beta}$'].sum()
             
-            self.make_cov_matrices(mode=mode)
-            cov_mat = self.hb_cov
+            hb_cov = self.make_cov_matrices(mode=mode)
+            cov_mat = hb_cov
             
         var = np.linalg.multi_dot([w_vec, cov_mat, w_vec.T])[0][0]
         
